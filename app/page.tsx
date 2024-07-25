811cca2d45c6dfacac64bac88786b84f3cd1ed17
dsampson94 <davesampson15@gmail.com>
2024-07-25 23:39:51 +0200

    progress

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
                 <Hero/>
                 <PrimaryFeatures/>
                 <TeachersProfiles/>
-                <Testimonials/>
                 <Faqs/>
             </main>
             <ContactSupportButton/>
