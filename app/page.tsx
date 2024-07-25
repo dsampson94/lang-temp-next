e727f645d3717e845045bfa8ce27c5ac683da7c4
dsampson94 <davesampson15@gmail.com>
2024-07-25 23:40:17 +0200

    progress

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Header from '../components/landing/Header';
 import { PrimaryFeatures } from '../components/landing/PrimaryFeatures';
 import { TeachersProfiles } from '../components/landing/TeachersProfiles';
 import { Faqs } from '../components/landing/Faqs';
-import { Testimonials } from '../components/landing/Testimonials';
 import { ContactSupportButton } from '../components/landing/ContactSupportButton';
 import { Analytics } from '@vercel/analytics/react';
 
